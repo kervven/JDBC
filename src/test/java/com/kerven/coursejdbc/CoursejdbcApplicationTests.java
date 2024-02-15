8802a70ee147cb403b2bdd458d7dc0810405c674
Kerven Kildhery <kervensilvva@gmail.com>
2024-02-15 18:47:12 -0300

    feita classe que contem conexões, desconexões e carregamentos dos dados do bd, classe de exceção do db e consultas sql.

---
+++ src/test/java/com/kerven/coursejdbc/CoursejdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerven.coursejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
